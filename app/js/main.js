e75b40ccf4bad159fcaaa854d2cab34331481db6
converse0 <kvasiuksergei2012@yandex.ua>
2021-12-03 17:27:44 +0300

    init

---
+++ app/js/main.js
@@ -0,0 +1,44 @@
+$(function () {
+  $('.products__slider').slick({
+    infinite: false,
+    prevArrow: `<button class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.21839 1L1 9L9.21839 17"/>
+    </svg></button>`,
+    nextArrow: `<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0.78161 17L9 9L0.78161 1"/>
+    </svg>
+    </button>`,
+  });
+
+  const removeItem = () => {
+    $('.questions__item').removeClass('questions__item--active');
+  };
+
+  $('.questions__item-title').on('click', function () {
+    if ($(this).parent().hasClass('questions__item--active')) {
+      removeItem();
+    } else {
+      removeItem();
+      $(this).parent().addClass('questions__item--active');
+    }
+  });
+
+  $('#fullpage').fullpage({
+    autoScrolling: true,
+    scrollHorizontally: true,
+    sectionSelector: '.page-section',
+    scrollOverflow: true,
+    menu: '#header__nav',
+    anchors: ['home', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__btn').toggleClass('menu__btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu__list-link').on('click', function () {
+    $('.menu__btn').removeClass('menu__btn--active');
+    $('.menu__list').removeClass('menu__list--active');
+  });
+});
